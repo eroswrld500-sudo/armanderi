3642904e500e65f858fe9ac93b20cf1b056c5d95
eroswrld500-sudo <eroswrld500@gmail.com>
2026-06-19 21:48:04 +0300

    Create products.js

---
+++ products.js
@@ -0,0 +1,18 @@
+export const products = [
+  {
+    id: 1,
+    name: "Classic Italian Briefcase",
+    price: 1250,
+    category: "Bags",
+    description: "El yapımı, %100 deri. Zamansız bir tasarım.",
+    image: "briefcase.jpg"
+  },
+  {
+    id: 2,
+    name: "Minimalist Leather Wallet",
+    price: 250,
+    category: "Accessories",
+    description: "Kompakt, zarif ve dayanıklı.",
+    image: "wallet.jpg"
+  }
+];
